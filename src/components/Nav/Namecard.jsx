f6f9c24167ae6d30c4d8fd3d101ea68c5c18b03a
Chris Chang <1chang1chang1@gmail.com>
2023-12-08 20:39:38 -0800

    Intermediate work

--- src/components/Nav/Namecard.jsx
+++ src/components/Nav/Namecard.jsx
@@ -2,11 +2,26 @@ export function Namecard() {
   return (
     <div
       id="namecard"
-      className="hidden sm:visible sm:basis-1/4 transition delay-150 duration-300 ease-in-out hover:scale-110 group-hover:-translate-y-1"
+      className="hidden transition delay-150 duration-300 ease-in-out hover:scale-110 group-hover:-translate-y-1 sm:visible sm:basis-1/4"
     >
-      <h1 className="text-source-code text-bold text-center text-title text-light transition delay-150 duration-300 ease-in-out hover:text-accent-100">
+      {/* <h1 className="text-source-code text-bold text-center text-title text-light transition delay-150 duration-300 ease-in-out hover:text-accent-100">
         &lt;CC/&gt;
       </h1>
+       */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="white"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
+        />
+      </svg>
     </div>
   );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,57 +7,64 @@ import { Namecard } from "./Namecard";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function Nav(props) {
-  const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
-    <>
-      <nav className="sticky top-0 z-50 flex h-16 flex-row items-center justify-between bg-accent-950 px-6">
-        <Namecard />
+    <nav className="fixed z-20 flex h-16 w-full items-center justify-between lg:justify-center bg-accent-950 px-10">
+      {/* <Namecard /> */}
 
-        <div
-          className="space-y-2 bg-accent-400"
-          onClick={() => {
-            console.log("hello world!");
-            setIsNavOpen((prev) => !prev);
-          }}
+      {/* hamburger icon */}
+      <button className="focus:outline-non inline-flex h-10 w-10 items-center justify-center rounded-md border outline-none lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="white"
+          className="h-6 w-6"
         >
-          <span className="block h-1 w-8 bg-primary-100"></span>
-          <span className="block h-1 w-8 bg-primary-100"></span>
-          <span className="block h-1 w-8 bg-primary-100"></span>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+      <div className="mt-10 hidden lg:mt-0 lg:inline-flex lg:w-auto ">
+        <div id="router" className="flex basis-1/2 text-light lg:flex-row">
+          <a href="#" className="w-32 flex-initial">
+            aboot me!
+          </a>
+          <a href="#" className="w-32 flex-initial">
+            hello world
+          </a>
+          <a href="#" className="w-32 flex-initial">
+            hello world
+          </a>
+          <a href="#" className="w-32 flex-initial">
+            hello world
+          </a>
         </div>
-
-        {/* <div id="router" className="basis-1/2">
-        <Router>
-          <div className="flex justify-around space-x-2 sm:flex-row">
+      </div>
+      {/* <div id="router" className="basis-1/2">
+          <Router>
             <Link route="about" />
             <Link route="experience" />
             <Link route="projects" />
             <Link route="contact" />
-          </div>
-        </Router>
-      </div> */}
-        <IconContext.Provider
-          id="icons"
-          value={{
-            color: "white",
-            className: "global-class-name",
-          }}
-        >
-          <button onClick={props.toggleDarkMode}>
-            <BsFillMoonStarsFill size={25} />
-          </button>
-        </IconContext.Provider>
-      </nav>
-      {isNavOpen ? <div className="sticky">
-        <Router>
-          <div className="flex-row">
-            <Link route="about" />
-            <Link route="experience" />
-            <Link route="projects" />
-            <Link route="contact" />
-          </div>
-        </Router>
-      </div> : null}
-    </>
+          </Router>
+        </div> */}
+      <IconContext.Provider
+        id="icons"
+        value={{
+          color: "white",
+          className: "global-class-name",
+        }}
+      >
+        <button onClick={props.toggleDarkMode}>
+          <BsFillMoonStarsFill size={25} />
+        </button>
+      </IconContext.Provider>
+    </nav>
   );
 }
 export default Nav;
